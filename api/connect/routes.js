4f81d4594ab263db0e4d4000e43ce62ea29fa522
Mateuspv <mateuspatzlaff@gmail.com>
2015-04-15 16:08:28 -0300

    update routes path

--- api/connect/routes.js
+++ api/connect/routes.js
@@ -3,12 +3,12 @@ var Post = require('./handlers/post');
 module.exports = [
   {
     method: 'GET',
-    path: '/connect/post',
+    path: '/connect/posts',
     handler: Post.all
   },
   {
     method: 'POST',
-    path: '/connect/post',
+    path: '/connect/posts',
     handler: Post.create
   },
 ]
