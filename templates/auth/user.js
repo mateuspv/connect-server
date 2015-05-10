4ee2ca670f7882cd2462722caf4f1a7e68a35f95
mateuspv <mateuspatzlaff@gmail.com>
2015-05-10 05:34:31 -0300

    update auth/status

--- templates/auth/user.js
+++ templates/auth/user.js
@@ -3,3 +3,6 @@ json.set('tweets', user.statuses_count);
 json.set('following', user.friends_count);
 json.set('follower', user.followers_count);
 json.set('profile_image', user.profile_image_url_https);
+json.set('description', user.description);
+json.set('profile_banner_url', user.profile_banner_url);
+
