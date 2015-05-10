ccc5ce3a7ee0bcd95df0b5077a92e026a8a611f5
mateuspv <mateuspatzlaff@gmail.com>
2015-05-10 06:13:53 -0300

    more λ like

---
+++ api/connect/handlers/_helpers.js
@@ -0,0 +1,49 @@
+/**
+ * extract from in a connect promise style
+ * where
+ *  data[0].data is facebook
+ *  data[1] is twitter
+ * @param  {Object}
+ * @return {Object}
+ */
+exports.extract = function (data) {
+	var facebook = data[0].data;
+	var twitter = data[1];
+
+	return {facebook: facebook, twitter: twitter};
+};
+
+/**
+ * concat a connect object to array
+ * @param  {Object}
+ * @return {Array}
+ */
+exports.concat = function (data) {
+	var result = [];
+
+	return result.concat(data.facebook, data.twitter);
+};
+
+/**
+ * responseWith puts data in a field
+ * @param  {String} fieldName
+ * @param  {data} Any
+ * @return {Object}
+ */
+exports.responseWith = function (fieldName, data) {
+	var resp = {};
+	resp[fieldName] = data;
+	return resp;
+};
+
+/**
+ * Applies a formater to each network
+ * @param  {Object} Formater
+ * @param  {Object}
+ * @return {Object}
+ */
+exports.format = function (Formater, data) {
+	var facebook = Formater.facebook(data.facebook);
+	var twitter = Formater.twitter(data.twitter);
+	return {facebook: facebook, twitter: twitter};
+};
--- api/connect/handlers/friends.js
+++ api/connect/handlers/friends.js
@@ -1,34 +1,31 @@
 var handler = require('./_handler');
 var Formater = require('./../_formater/index').Friends;
-var Promise = require('es6-promise').Promise;
+var Helpers = require('./_helpers');
 
-var extract = function (friends) {
-	return {facebook: friends[0].data, twitter: friends[1]};
-};
+var R = require('ramda');
+var curry = R.curry;
+var curryN = R.curryN;
 
-var format = function (friends) {
-	var facebook = Formater.facebook(friends.facebook);
-	var twitter = Formater.twitter(friends.twitter);
-	return {facebook: facebook, twitter: twitter};
-}
-
-var concat = function (friends) {
-	var result = [];
-	return result.concat(friends.facebook, friends.twitter);
-}
-
-var response = function (reply) {
-	return function (friends) {
-		return reply({friends: friends})
-	}
-}
+/**
+ * API 
+ */
 
 exports.all = handler(function (connect, request, reply) {
-  var Friends = connect.Friends;
+	var Friends = connect.Friends;
+	var result = Friends.all();
 
-	Friends.all()
-		.then(extract)
-		.then(format)
-		.then(concat)
-		.then(response(reply));
+	result
+		.then(Helpers.extract)
+		.then(applyFormater)
+		.then(Helpers.concat)
+		.then(responseWithFriends)
+		.then(curryN(1, reply));
 });
+
+/**
+ * Privte
+ */
+
+var applyFormater = curry(Helpers.format)(Formater);
+
+var responseWithFriends = curry(Helpers.responseWith)('friends');
